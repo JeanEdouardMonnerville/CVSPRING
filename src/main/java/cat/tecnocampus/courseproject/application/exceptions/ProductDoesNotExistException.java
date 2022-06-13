a16f6dc8c05e7297cbcfba8458c37e985826d4e7
jemon <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-06-13 14:10:37 +0200

    OK

--- src/main/java/cat/tecnocampus/courseproject/application/exceptions/ProductDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package cat.tecnocampus.courseproject.application.exceptions;
-
-public class ProductDoesNotExistException extends RuntimeException{
-    public ProductDoesNotExistException(String product) {
-        super("The product " + product + " does not exist");
-    }
-}
--- src/main/java/cat/tecnocampus/courseproject/application/exceptions/UserDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package cat.tecnocampus.courseproject.application.exceptions;
-
-public class UserDoesNotExistException extends RuntimeException {
-    public UserDoesNotExistException(String name) {
-        super("User " + name + " doesn't exist");
-    }
-}
